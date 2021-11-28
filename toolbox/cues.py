9c35eb1e404084d659350f6895cc492a80a80b9d
Gursimran Singh <75020691+haubergeon@users.noreply.github.com>
2021-11-29 00:30:05 +0530

    added code for both right and left stimulus

--- toolbox/cues.py
+++ toolbox/cues.py
@@ -1,18 +1,41 @@
+import time
 import os
 from tkinter import Tk,Canvas,PhotoImage
 from PIL import Image, ImageTk
+# time in seconds, make sure time in record.py matches this
+time_period = 3
 
-root = Tk()
-root.geometry("+200+200")
+def display_left():
+    root = Tk()
+    root.geometry("+200+200")
+    canvas = Canvas(root, width = 300, height = 300)  
+    canvas.pack()  
+    img = ImageTk.PhotoImage(Image.open("left.png"))  
+    canvas.create_image(20,20, anchor='nw', image=img)
+    root.after(3000,lambda:root.destroy())
+    #os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
+    root.mainloop()
 
-canvas = Canvas(root, width = 300, height = 300)  
-canvas.pack()  
-img = ImageTk.PhotoImage(Image.open("left.png"))  
-canvas.create_image(20,20, anchor='nw', image=img) 
+display_left()
 
 
-os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
-root.mainloop()
+time.sleep(time_period)
+
+def display_right():
+    root = Tk()
+    root.geometry("+1000+200")
+    canvas = Canvas(root, width = 300, height = 300)  
+    canvas.pack()  
+    img = ImageTk.PhotoImage(Image.open("right.png"))  
+    canvas.create_image(20,20, anchor='nw', image=img) 
+    #os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
+    root.after(3000,lambda:root.destroy())
+    root.mainloop()
+
+display_right()
+
+time.sleep(time_period)
+
 
 
 
--- toolbox/readme.md
+++ toolbox/readme.md
@@ -4,3 +4,6 @@ pip install pillow
 
 ## live-plot.py
 Plots the data from a csv file.
+
+## cues.py
+Displays each cue for a set timeperiod and records data in csv for that time period.

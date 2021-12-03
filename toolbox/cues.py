8445e3960eb534645a027683fa2bc8c06a519d08
Gursimran Singh <75020691+haubergeon@users.noreply.github.com>
2021-12-03 19:13:47 +0530

    added eeg csvs

--- toolbox/cues.py
+++ toolbox/cues.py
@@ -2,8 +2,8 @@ import time
 import os
 from tkinter import Tk,Canvas,PhotoImage
 from PIL import Image, ImageTk
-# time in seconds, make sure time in record.py matches this
-time_period = 3
+# time between left and right
+time_period = 10
 
 def display_left():
     root = Tk()
@@ -12,9 +12,9 @@ def display_left():
     canvas.pack()  
     img = ImageTk.PhotoImage(Image.open("left.png"))  
     canvas.create_image(20,20, anchor='nw', image=img)
-    root.after(3000,lambda:root.destroy())
-    #os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
+    root.after(5000,lambda:root.destroy())
     root.mainloop()
+    os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
 
 display_left()
 
@@ -28,9 +28,9 @@ def display_right():
     canvas.pack()  
     img = ImageTk.PhotoImage(Image.open("right.png"))  
     canvas.create_image(20,20, anchor='nw', image=img) 
-    #os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
-    root.after(3000,lambda:root.destroy())
+    root.after(5000,lambda:root.destroy())
     root.mainloop()
+    os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
 
 display_right()
 

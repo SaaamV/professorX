2ffb3df3b836d3a6baf6b5716d946c16972bc604
Gursimran Singh <75020691+haubergeon@users.noreply.github.com>
2021-12-05 19:45:38 +0530

    minor changes

--- toolbox/seq.py
+++ toolbox/seq.py
@@ -1,10 +1,15 @@
-
 import numpy as np
+import pandas as pd
+
 # total number of elements
 n = 120
 # k is the number of zeros
 k = 60
 arr = np.array([0]*k + [1]*(n-k))
 np.random.shuffle(arr)
+
+df = pd.DataFrame(arr, columns=['Sequence'])
+
+df.to_csv('sequence.csv')
 
 

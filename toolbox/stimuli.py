1fd78815bf8d2fbb9d629d0130b6b960c0c128e6
Sherlot <53267574+Sherlot@users.noreply.github.com>
2021-12-06 10:34:43 +0530

    Edited code to randomize the left and right cues

--- toolbox/stimuli.py
+++ toolbox/stimuli.py
@@ -2,57 +2,43 @@ import time
 import os
 from tkinter import Tk,Canvas,PhotoImage
 import random
-from PIL import Image, ImageTk
+import pandas as pd
 # time in seconds, make sure time in record.py matches this
-time_period = 3
-# time period of change from left to right
-direction_time_period = 5
-# total time of display of one direction
-color_time_period = random.choice(range(1000,4000,1000))
-# total time of display of one color
+colors_list = ["red", "yellow", "green"]
+
+df = pd.read_csv(r'C:\Users\Tushar\Desktop\ProfessorX\sequence.csv')
+# change the location of the csv file as per system
+markers = df["Sequence"].to_numpy()
+# imports the binary values from sequence.csv into a list named markers
 
 def display_left():
-    colors_list = ["red", "yellow", "green", "blue"]
-    timeout = time.time() + direction_time_period
-    for i in colors_list:
-        if time.time() > timeout:
-            break
-        else:
-            root = Tk()
-            root.geometry("+100+200")
-            canvas = Canvas(root, width=300, height=300)
-            canvas.pack()
-            canvas.create_rectangle(0, 0, 300, 300, fill=i)
-            canvas.create_line(75, 150, 225, 150, fill="black", width=10)
-            canvas.create_line(75, 150, 150, 100, fill="black", width=10)
-            canvas.create_line(75, 150, 150, 200, fill="black", width=10)
-            #os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
-            root.after(color_time_period, lambda: root.destroy())
-            root.mainloop()
-
-display_left()
-
-time.sleep(time_period)
+    color_of_frame = random.choice(colors_list)
+    root = Tk()
+    root.geometry("+100+200")
+    canvas = Canvas(root, width=300, height=300)
+    canvas.pack()
+    canvas.create_rectangle(0, 0, 300, 300, fill=color_of_frame)
+    canvas.create_line(75, 150, 225, 150, fill="black", width=10)
+    canvas.create_line(75, 150, 150, 100, fill="black", width=10)
+    canvas.create_line(75, 150, 150, 200, fill="black", width=10)
+    root.after(1000, lambda: root.destroy())
+    root.mainloop()
 
 def display_right():
-    colors_list = ["red", "yellow", "green", "blue"]
-    timeout = time.time() + direction_time_period
-    for i in colors_list:
-        if time.time() > timeout:
-            break
-        else:
-            root = Tk()
-            root.geometry("+1100+200")
-            canvas = Canvas(root, width = 300, height = 300)
-            canvas.pack()
-            canvas.create_rectangle(0,0,300,300,fill=i)
-            canvas.create_line(75,150,225,150,fill="black",width=10)
-            canvas.create_line(225,150,150,100,fill="black",width=10)
-            canvas.create_line(225,150,150,200,fill="black",width=10)
-            #os.system("python C:/Users/KHALS/OneDrive/Desktop/professorX/record.py")
-            root.after(color_time_period,lambda:root.destroy())
-            root.mainloop()
+    color_of_frame = random.choice(colors_list)
+    root = Tk()
+    root.geometry("+1100+200")
+    canvas = Canvas(root, width = 300, height = 300)
+    canvas.pack()
+    canvas.create_rectangle(0,0,300,300,fill=color_of_frame)
+    canvas.create_line(75,150,225,150,fill="black",width=10)
+    canvas.create_line(225,150,150,100,fill="black",width=10)
+    canvas.create_line(225,150,150,200,fill="black",width=10)
+    root.after(1000,lambda:root.destroy())
+    root.mainloop()
 
-display_right()
-
-time.sleep(time_period)
+for i in markers:
+    if i==0:
+        display_left()
+    else:
+        display_right()
